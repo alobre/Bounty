33d452a48d76fe61380a553d12fbd46a803181da
alobre <51901804+alobre@users.noreply.github.com>
2020-12-14 13:20:17 +0100

    Using react-native-paper and added Tabs
    +using react-native-paper
    +added tabs via react-native-tab-view

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import React from 'react';
-import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList, Alert, Dimensions} from 'react-native';
 import TaskCard from './components/TaskCard';
+import { Provider as PaperProvider } from 'react-native-paper';
 import {Container} from 'native-base'
+import { TabView, SceneMap } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <TaskCard username="alobre" title="Kleiner Einkauf" category="Einkauf" description={"alo"} wage="5€"/>
+);
+const SecondRoute = () => (
+  <View style={ {backgroundColor: '#673ab7'} } />
+);
+const initialLayout = { width: Dimensions.get('window').width };
 
 const App = () => {
+  // const [item, setItem] = useState("Hallo, Ich bräuchte: 3x Äpfel, Tomaten, Faschiertes Rind, Käse")
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
 
+  const renderScene = SceneMap({
+    first: FirstRoute,
+    second: SecondRoute,
+  });
+
+  // let desctipton="Hallo, Ich bräuchte: 3x Äpfel, Tomaten, Faschiertes Rind, Käse"
   return (
-    <Container>
-      <TaskCard username="alobre" title="Kleiner Einkauf" category="Einkauf" description="Hallo, ich bräuchte:" wage="5€"/>
-    </Container>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={initialLayout}
+    />
+    // <PaperProvider>
+    // <Container>
+    //   <TaskCard username="alobre" title="Kleiner Einkauf" category="Einkauf" description={item} wage="5€"/>
+    // </Container>
+    // </PaperProvider>
   )
 }
 
--- components/TaskCard.js
+++ components/TaskCard.js
@@ -1,49 +1,81 @@
 import React, { Component } from 'react';
-import {StyleSheet} from 'react-native'
+import {StyleSheet, View} from 'react-native'
 import globalStyles from './GlobalStyles.js'
-import { Left, Thumbnail, Container, Header, Text, Content, Card, CardItem, Body } from 'native-base';
+import { Avatar, Button, Card, Title, Paragraph, Text, Subheading, Divider, TouchableRipple } from 'react-native-paper';
 
+// import { Left, Thumbnail, Container, Header, Text, Content, Card, CardItem, Body } from 'native-base';
 
 const TaskCard = ({username, title, category, description, wage}) =>{
   return (
-      <Container>
-        <Header />
-        <Content>
-          <Card>
-            <CardItem>
-              <Text header style={globalStyles.fontSansBold}>{title}</Text>
-            </CardItem>
-          <CardItem>
-              <Left>
-                <Thumbnail small source={require('../media/obama.jpg')} />
-                <Body>
-                  <Text>{username}</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Text note>{category}</Text>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Text style={globalStyles.fontSerif}>
-                  {description}
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem footer bordered>
-              <Text style={globalStyles.fontSans}>{wage}</Text>
-            </CardItem>
-         </Card>
-        </Content>
-      </Container>
+    <Card elevation={4}>
+    <Card.Title title={title} subtitle={category}/>
+    <Card.Content>
+      <View style={styles.profile}>
+        <Avatar.Image size={24} source={require('../media/obama.jpg')} />
+        <Subheading style={styles.username}>{username}</Subheading>
+      </View>
+      <Divider/>
+      <Paragraph style={globalStyles.fontSerif}>{description}</Paragraph>
+    </Card.Content>
+    <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+    <TouchableRipple rippleColor="rgba(0, 0, 0, .32)">
+      <Card.Actions style={styles.buttonParent}>
+        <Button onPress={() => console.log('Pressed')}>Auftraggeber kontaktieren</Button>
+      </Card.Actions>
+    </TouchableRipple>
+  </Card>
+      // <Container>
+      //   <Header />
+      //   <Content>
+      //     <Card style={styles.card}>
+      //       <CardItem style={styles.title}>
+      //         <Text header style={globalStyles.fontSansBold}>{title}</Text>
+  
+      //       </CardItem>
+      //     <CardItem style={styles.profile}>
+      //       <Left>
+      //         <Thumbnail small source={require('../media/obama.jpg')} />
+      //         <Body>
+      //           <Text>{username}</Text>
+      //         </Body>
+      //           </Left>
+      //     </CardItem>
+      //       <CardItem>
+      //         <Text note>{category}</Text>
+      //       </CardItem>
+      //       <CardItem>
+      //         <Body>
+      //           <Text style={globalStyles.fontSerif}>
+      //             {description}
+      //           </Text>
+      //         </Body>
+      //       </CardItem>
+      //       <CardItem footer bordered>
+      //         <Text style={globalStyles.fontSans}>{wage}</Text>
+      //       </CardItem>
+      //    </Card>
+      //   </Content>
+      // </Container>
     );
   }
 
   const styles = StyleSheet.create({
-    font:{
-      fontFamily: "Cochin",
-      color: 'red'
+    card:{
+      borderRadius: 1
+    },
+    title:{
+      flex: 3,
+      paddingBottom:0
+    },
+    profile:{
+      flexDirection: "row",
+    },
+    username:{
+      paddingLeft: 3,
+      color: "#0a90c9"
+    },
+    buttonParent:{
+      justifyContent: "flex-end"
     }
   })
 
