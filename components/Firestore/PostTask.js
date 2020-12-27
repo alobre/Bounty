b22f2487b1ceb08564cb6627da755c02b4a78c34
alobre <alobre@users.noreply.github.com>
2020-12-27 16:03:47 +0100

    Added Tags
    + Added Tags

--- components/Firestore/PostTask.js
+++ components/Firestore/PostTask.js
@@ -1,13 +1,13 @@
 import { View } from 'native-base';
-import React, { Component } from 'react';
+import React, { Component, setState } from 'react';
 import { StyleSheet } from 'react-native';
-import { TextInput, Button, Chip } from 'react-native-paper';
+import { TextInput, Button, Chip, Text, Snackbar } from 'react-native-paper';
 import { Textarea, Form, Item, Input } from "native-base";
 import firestore from '@react-native-firebase/firestore';
 import { firebase } from '@react-native-firebase/auth';
 import auth from '@react-native-firebase/auth';
 
-function StoreTask(title, description){
+function StoreTask(title, description, tags){
     console.log(auth().currentUser.uid)
     firestore()
     .collection('tasks')
@@ -16,7 +16,8 @@ function StoreTask(title, description){
         {
             '1':{
                 'title': title,
-                'description': description
+                'description': description,
+                'tags': tags
             }
         }
         )
@@ -25,27 +26,38 @@ function StoreTask(title, description){
     }); 
 }
 
-function addChipForeachTag(){
-    let totalChips = this.state.tags;
-    return(
-    <Chip>{totalChips[0]}</Chip>
-    )
-}
+
 
 export default class PostTask extends Component{
     constructor(props){
         super(props);
+        this.addTagToTags = this.addTagToTags.bind(this)
+        this.removeTagFromTags = this.removeTagFromTags.bind(this)
         this.state={
             title:'',
             description:'',
             currentTag: '',
-            tags:[]
+            tags:[],
+            chipWarning: false
         }
     }
 
     addTagToTags(tag){
-        this.state.tags = [...this.state.tags, tag]
+        if(this.state.tags.length < 3){
+            this.setState({tags: [...this.state.tags, tag]})
+        } else {
+            this.toggleChipWarning()
+        }
+        
     }
+    removeTagFromTags(tag){
+        let newList = this.state.tags.filter(el => el != tag)
+        this.setState({tags: newList})
+    }
+
+    toggleChipWarning = () => this.setState({chipWarning: true});
+
+    dismissChipWarning = () => this.setState({chipWarning: false});
 
     render(){
         return(
@@ -56,14 +68,35 @@ export default class PostTask extends Component{
                 <Form>
                     <Textarea onChangeText={text=> this.state.description = text} rowSpan={12} bordered placeholder="Beschreibung..." />
                 </Form>
-                {addChipForeachTag}
+                <View style={styles.tagParent}>
+                    <Button onPress={()=> console.log(this.state.tags)}>press</Button>
+                    {this.state.tags.map(tag => <Chip key={tag}  onClose={ ()=> this.removeTagFromTags(tag) } closeIconAccessibilityLabel="close" mode="outlined">{tag}</Chip>)}
+                    <Snackbar
+                    visible={this.state.chipWarning}
+                    onDismiss={this.dismissChipWarning}
+                    action={{
+                    label: 'Ok',
+                    }}>
+                    Es sind leider nur maximal 3 Schlagwörter erla  ubt.
+                </Snackbar>
+                </View>
                 <Item>
-                    <Input onChangeText={text=> this.state.currentTag = text} onSubmitEditing={ ()=> {this.addTagToTags(this.state.currentTag); console.log(this.state.tags)}} placeholder="Titel" />
+                    <Input onChangeText={text=> this.state.currentTag = text} onSubmitEditing={ ()=> this.addTagToTags(this.state.currentTag)} placeholder="Schlagwörter" />
                 </Item>
-                <Button icon="send" mode="contained" onPress={()=> StoreTask(this.state.title, this.state.description)}>
+                <Button icon="send" mode="contained" onPress={()=> StoreTask(this.state.title, this.state.description, this.state.tags)}>
                     Auftrag aufgeben
                 </Button>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    tagParent:{
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+    },
+    tag:{
+        // width: this.state.chipWidth
+    }
+})
